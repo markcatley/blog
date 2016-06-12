394520cc4d77f3bbc821f49f36b903a62908ff67
Mark Catley <mark@catley.net.nz>
2016-06-12 13:13:47 +1200

    Upgrading to v0.8.0

--- core/server/storage/local-file-store.js
+++ core/server/storage/local-file-store.js
@@ -1,15 +1,15 @@
 // # Local File System Image Storage module
 // The (default) module for storing images, using the local file system
 
-var express   = require('express'),
-    fs        = require('fs-extra'),
-    path      = require('path'),
-    util      = require('util'),
-    Promise   = require('bluebird'),
-    errors    = require('../errors'),
-    config    = require('../config'),
-    utils     = require('../utils'),
-    baseStore = require('./base');
+var serveStatic = require('express').static,
+    fs          = require('fs-extra'),
+    path        = require('path'),
+    util        = require('util'),
+    Promise     = require('bluebird'),
+    errors      = require('../errors'),
+    config      = require('../config'),
+    utils       = require('../utils'),
+    baseStore   = require('./base');
 
 function LocalFileStore() {
 }
@@ -42,7 +42,8 @@ LocalFileStore.prototype.save = function (image, targetDir) {
 
 LocalFileStore.prototype.exists = function (filename) {
     return new Promise(function (resolve) {
-        fs.exists(filename, function (exists) {
+        fs.stat(filename, function (err) {
+            var exists = !err;
             resolve(exists);
         });
     });
@@ -51,7 +52,8 @@ LocalFileStore.prototype.exists = function (filename) {
 // middleware for serving the files
 LocalFileStore.prototype.serve = function () {
     // For some reason send divides the max age number by 1000
-    return express['static'](config.paths.imagesPath, {maxAge: utils.ONE_YEAR_MS});
+    // Fallthrough: false ensures that if an image isn't found, it automatically 404s
+    return serveStatic(config.paths.imagesPath, {maxAge: utils.ONE_YEAR_MS, fallthrough: false});
 };
 
 module.exports = LocalFileStore;
